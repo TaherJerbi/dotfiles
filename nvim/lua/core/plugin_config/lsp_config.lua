66b8f0015912cc1786eacacbe0b85d0b8f81f251
Taher Jerbi <jerbi.taherr@gmail.com>
2024-01-17 19:30:19 +0100

    add lsp ensure_installed

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,17 +1,24 @@
-local lsp = require('lsp-zero').preset({})
+local lsp_zero = require('lsp-zero')
 
-lsp.on_attach(function(_, bufnr)
+lsp_zero.on_attach(function(_, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp.default_keymaps({
+  lsp_zero.default_keymaps({
     buffer = bufnr,
     preserve_mappings = false,
   })
 end)
 
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'tsserver', 'rust_analyzer'},
+  handlers = {
+    lsp_zero.default_setup,
+  }
+})
 
 -- Keybindings
 vim.keymap.set('n', '<leader>ca', ':CodeActionMenu<CR>')
 vim.keymap.set('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
 
-lsp.setup()
+lsp_zero.setup()
