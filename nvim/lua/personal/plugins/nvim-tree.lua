de34cb8f3278e6d70c23a02ffcd88bc4549b2038
TaherJerbi <jerbi.taherr@gmail.com>
2026-08-24 21:34:58 -0400

    Add nvim-tree

---
+++ nvim/lua/personal/plugins/nvim-tree.lua
@@ -0,0 +1,11 @@
+-- Disable netrw, as recommended by nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.pack.add { 'https://github.com/nvim-tree/nvim-tree.lua' }
+
+require('nvim-tree').setup {
+  view = { side = 'right' },
+}
+
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file [E]xplorer' })
