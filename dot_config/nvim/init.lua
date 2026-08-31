d7a7d6ee9ab2468f003cb9aea659c78c3020a01e
Taher Jerbi <jerbi.taherr@gmail.com>
2026-08-31 15:58:25 -0400

    Fix clipboard sync mac

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -118,12 +118,6 @@ do
   -- Don't show the mode, since it's already in the status line
   vim.o.showmode = false
 
-  -- Sync clipboard between OS and Neovim.
-  --  Schedule the setting after `UiEnter` because it can increase startup-time.
-  --  Remove this option if you want your OS clipboard to remain independent.
-  --  See `:help 'clipboard'`
-  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
-
   -- Enable break indent
   vim.o.breakindent = true
 
